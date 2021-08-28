85876486b126b100d6664b6a4075aa18f7ad47f7
KseniyaNakaryakova <narciss-lotos@yandex.ru>
2021-08-28 16:44:39 +0400

    Lesson1

---
+++ src/components/message/message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import './message.css';
+
+export const Message = (props) => {
+    return (
+        <h3> { props.text } </h3>
+    )
+};
