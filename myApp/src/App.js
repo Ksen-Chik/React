11b0556060b8c91eef5b4ffe1e5e9f3b693fdb55
KseniyaNakaryakova <narciss-lotos@yandex.ru>
2021-09-02 16:41:03 +0400

    ДЗ к уроку 2

--- myApp/src/App.js
+++ myApp/src/App.js
@@ -1,15 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import { Message } from './components/message/message.js';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  const [messageAuthor, setMessageAuthor] = useState("");
+  const [messageText, setMessageText] = useState("");
+  const [messageId, setMessageId] = useState(0);
+
+  const sendMessage = (msgAuthor, msgText) => {
+    if (messageAuthor === "" || messageText === "") {
+      return;
+    }
+    setMessageId(messageId + 1);
+    setMessages([...messages, { author: msgAuthor, text: msgText, id: messageId }]);
+  }
+
+  useEffect(() => {
+    setTimeout(function () {
+      let lastMessage = messages[messages.length - 1];
+      if (lastMessage !== undefined && lastMessage.author !== "Robot") {
+        sendMessage("Robot", `${lastMessage.author}, ваш звонок очень важен для вас`);
+      }
+    }, 1500);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3> My First React App </h3>
-        <Message text='Hello! My name is Ksenia)))' />
-      </header>
+      <div>
+        {messages.map((message) =>
+          <div key={message.id}>
+            <div>{message.author}: {message.text}</div>
+          </div>)}
+
+        <div>
+          <label>Автор: </label>
+          <input type="text" name="author" value={messageAuthor}
+            onChange={(event) => { setMessageAuthor(event.target.value) }}></input>
+
+          <label> Сообщение: </label>
+          <input type="text" name="message" value={messageText}
+            onChange={(event) => { setMessageText(event.target.value) }}></input>
+
+          <button onClick={() => sendMessage(messageAuthor, messageText)}>Отправить</button>
+        </div>
+      </div>
+
     </div>
   );
 }
--- myApp/src/components/message/message.js
+++ myApp/src/components/message/message.js
@@ -4,6 +4,29 @@ import './message.css';
 
 export const Message = (props) => {
     return (
-        <h3> { props.text } </h3>
+        <h3> {props.text} </h3>
     )
+};
+
+export const MessagesList = () => {
+    const [messages, setMessages] = useState([]);
+
+    const handleChange = (event) => {
+        /*setMessages([...messages, {messages.text: event.target.value, }]);*/
+    }
+
+    return (
+        <div>
+            {messages.map((message) =>
+                <div>
+                    <div>{message.text}</div>
+                    <div>{message.author}</div>
+                </div>)}
+
+            <div>
+                <input type="text" ></input>
+                <input type="text" ></input>
+            </div>
+        </div>
+    );
 };
